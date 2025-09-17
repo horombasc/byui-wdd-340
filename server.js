4af2010f8d2dcea0b1b47bd61184ccf8a5728ce1
horombac <collinhoromba@icloud.com>
2025-09-17 21:41:10 +0200

    commit the errors handling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities"); // Make sure this is imported!
 
 // View Engine
 app.set("view engine", "ejs");
@@ -14,12 +15,33 @@ app.set("layout", "./layouts/layout");
 
 // Routes
 app.use(static);
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 app.use("/inv", inventoryRoute);
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
-// Server Port
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+// Local Server Information
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+module.exports = app;
 
